c4579f46a0b80a0e058cd5bee42641e6dd711246
moonyoungsik95 <moonyoungsik95@gmail.com>
2025-01-10 17:50:47 +0900

    C:/Program Files/Git/home/increase 요청처리

--- src/main/java/com/mys/tutorial1/controller/HomeController.java
+++ src/main/java/com/mys/tutorial1/controller/HomeController.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 해당 클래스는 컨트롤러야
 // 이 클래스는 웹 요청을 받아서 작업할 거야
 public class HomeController {
+    int num;
+
+    public HomeController(){
+        //Controller 어노테이션이 들어가면 알아서 객체 생성되기 때문에 바로 쓸 수 있다.
+        num = -1;
+    }
+
     @GetMapping("/home/main")
     // 개발자가 스프링부트한테 말한다
     // 만약에 '/home/main'  요청이 들어오면 아래 매서드를 실행시켜줘
@@ -28,5 +35,14 @@ public class HomeController {
     @ResponseBody
     public String showHome3(){
         return "스프링부트는 획기적입니다.";
+    }
+
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease(){
+        num++;
+        return num;
+        // 우리는 숫자라 했지만 스프링부트에서 응답한 결과를 바디에 보내줄 때는 모든것이 문자열로 보여진다.
     }
 }
