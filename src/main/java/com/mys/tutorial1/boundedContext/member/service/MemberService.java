179660225f8a335b5e961fb4c4e1feeb33a2dea9
moonyoungsik95 <moonyoungsik95@gmail.com>
2025-01-16 17:48:56 +0900

    테스트

--- src/main/java/com/mys/tutorial1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/mys/tutorial1/boundedContext/member/service/MemberService.java
@@ -4,9 +4,9 @@ import com.mys.tutorial1.base.rsData.RsData;
 import com.mys.tutorial1.boundedContext.member.dto.Member;
 import com.mys.tutorial1.boundedContext.member.repository.MemberRepository;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+// 테스트
 // 아래 클래스는 Ioc 컨테이너에 의해 생사소멸이 결정된다.
 @Service//@Component : @Service와 같은 의미, 가독성 때문에 이렇게 표기
 @Data
